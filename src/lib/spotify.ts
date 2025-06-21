2ee6089f358f6a48ac4bedb3465a85770003e7af
Tomás Trincado <tomas@Tomass-iMac-Pro.local>
2025-06-21 02:23:02 -0400

    rm commented

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -20,37 +20,6 @@ export async function getSpotifyToken(): Promise<string> {
   return res.data.access_token;
 }
 
-// export async function getTopTracks(): Promise<any[]> {
-//   const token = await getSpotifyToken();
-
-//   // The Weeknd
-//   const artistId = '1Xyo4u8uXC1ZmMpatF05PJ';
-
-//   const res = await axios.get(
-//     `https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=US`,
-//     {
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//       },
-//     }
-//   );
-
-//   const tracks = res.data.tracks;
-
-//   const enriched = await Promise.all(
-//     tracks.map(async (track: any) => {
-//       if (!track.preview_url) {
-//         const fallback = await findPreviewUrl(track.name, track.artists[0]?.name ?? '');
-//         // console.log(`[Fallback] ${track.name}: ${fallback}`);
-//         return { ...track, preview_url: fallback };
-//       }
-//       return track;
-//     })
-//   );
-
-//   return enriched;
-// }
-
 export async function getTopTracksFromArtist(artistId: string): Promise<any[]> {
   const token = await getSpotifyToken();
 
